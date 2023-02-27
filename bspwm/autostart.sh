eec48ab049e8a51eced71ce9016b38c42cf9924c
Lubos Noska <lubos.noska12@gmail.com>
2023-02-27 17:34:25 +0100

    Add Bspwm new colors

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -10,3 +10,4 @@ feh --bg-fill --randomize /usr/share/backgrounds/wallpapers/*
 picom -b &
 xrandr --output eDP --mode 1920x1080 --pos 0x0 --rotate normal &
 
+echo 10 > /sys/class/backlight/acpi_video0/brightness
